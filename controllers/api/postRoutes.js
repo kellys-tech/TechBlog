eaa90a068cd52ce4fd6ae1f38b7020f34e451dcc
Kelly Smith <kdalton77@hotmail.com>
2021-01-02 16:37:21 -0500

    added bootstrap

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
-const { Post } = require('../../models');
-
+const { Post, User } = require('../../models');
 
 // CREATE post
 router.post('/', async (req, res) => {
